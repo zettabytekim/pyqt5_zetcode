e1f4e5a547114aeee0ede9af7fdf565e7dbfe0f2
Zetta Kim <zettabyte.kim@gmail.com>
2019-01-24 17:39:11 +0900

    directory scan 2 일부 테스트

--- directoryScan2.py
+++ directoryScan2.py
@@ -1,16 +1,23 @@
 import os.path
+import pandas as pd
 
 folder = os.getcwd()
 
 print('Current folder : %s' % folder)
 
-for path, dirs, files in os.walk(folder):
+df = pd.DataFrame(columns=('directory', 'file', 'full_name'))
+
+i = 0
+for path, dirs, files in os.walk('/Test'):
     print('\nFolder: ', path)
 
     if files:
         for filename in files:
  #           print(' Files: ', os.path.join(path, filename))
-            print(' Files: ', filename)
+ #           print(' Files: ', filename)
+            df = [path, filename]
+            i = I + 1
 
+df.head()
 
 exit(0)
