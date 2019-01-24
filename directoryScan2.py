8bee5c13bee0474c93246de89580a024ca954e23
Zetta Kim <zettabyte.kim@gmail.com>
2019-01-24 17:48:30 +0900

    Update directoryScan2.py

--- directoryScan2.py
+++ directoryScan2.py
@@ -15,9 +15,8 @@ for path, dirs, files in os.walk('/Test'):
         for filename in files:
  #           print(' Files: ', os.path.join(path, filename))
  #           print(' Files: ', filename)
-            df = [path, filename]
-            i = I + 1
-
+            df2 = pd.DataFrame([path, filename], columns=('directory', 'file', 'full_name'))
+            df.append(df2, ignore_index=True)
 df.head()
 
 exit(0)
